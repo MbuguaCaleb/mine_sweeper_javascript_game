85d38781177d1163d032f05429ca40b5ef041cfc
MbuguaCaleb <mbuguacaleb30@gmail.com>
2020-07-27 18:16:14 +0300

    beginning-vanilla-js-breakout-game

---
+++ app.js
@@ -0,0 +1,47 @@
+/*The purpose of this is that all our Html is loaded before loading any javascript*/
+/*Alternative is passing my script tag after my javascript*/
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  let width = 10;
+  let bombAmount = 20;
+  let squares = [];
+
+  //create Board
+  //Creates 100 squares through looping through and adding a unique id*/
+  function createBoard() {
+    //get shuffield game array with random bombs
+    /**
+     * creates an Array of 20 Elements and fills them
+     * with a random value bomb.
+     *
+     * Array fill fills the array elements with a static value
+     */
+    const bombsArray = Array(bombAmount).fill('bomb');
+    const emptyArray = Array(width * width - bombAmount).fill('valid');
+
+    /**
+     *
+     * The concat() method is used to join two or more arrays.
+     * This method does not change the existing arrays, but returns a new array,
+     *  containing the values of the joined arrays.
+     */
+    const gameArray = emptyArray.concat(bombsArray);
+
+    /*Randomly Modelling elements of the New Array*/
+    const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+
+    for (let i = 0; i < width * width; i++) {
+      const square = document.createElement('div');
+      square.setAttribute('id', i);
+      /*Adding classnames to the sqaures array from the result of the shuffled Array*/
+      square.classList.add(shuffledArray[i]);
+      grid.appendChild(square);
+      squares.push(square);
+    }
+
+    /*Adding numbers to the neigbouring sqaures of the bomb*/
+  }
+
+  /*calling the create board function*/
+  createBoard();
+});
---
+++ readme.md
@@ -0,0 +1,10 @@
+**What is recursion**
+
+```
+
+It is a function that can be able to call itself until it cannot call itself
+anymore.
+
+Eg it will keep executing until it is able to achieve its required results.
+
+```
