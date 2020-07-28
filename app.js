fe6487a4798e03ce93042e6b5e7f4d43e4d1051b
MbuguaCaleb <mbuguacaleb30@gmail.com>
2020-07-28 06:11:48 +0300

    calculate-total-surrrounding-bombs

--- app.js
+++ app.js
@@ -37,11 +37,66 @@ document.addEventListener('DOMContentLoaded', () => {
       square.classList.add(shuffledArray[i]);
       grid.appendChild(square);
       squares.push(square);
+
+      //normal click
+      square.addEventListener('click', function (e) {
+        click(square);
+      });
     }
 
     /*Adding numbers to the neigbouring sqaures of the bomb*/
+    for (let i = 0; i < squares.length; i++) {
+      /*checking if the square is at the left edge*/
+      let total = 0;
+      const isLeftEdge = i % width === 0;
+      const isRightEdge = i === width - 1;
+
+      if (squares[i].classList.contains('valid')) {
+        if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb'))
+          total++;
+        if (
+          i > 9 &&
+          !isRightEdge &&
+          squares[i + 1 - width].classList.contains('bomb')
+        )
+          total++;
+
+        if (i > 10 && squares[i - width].classList.contains('bomb')) total++;
+        if (
+          i > 11 &&
+          !isLeftEdge &&
+          squares[i - 1 - width].classList.contains('bomb')
+        )
+          total++;
+        if (i < 98 && !isRightEdge && squares[i + 1].classList.contains('bomb'))
+          total++;
+        if (
+          i < 90 &&
+          !isLeftEdge &&
+          squares[i - 1 + width].classList.contains('bomb')
+        )
+          total++;
+        if (
+          i < 88 &&
+          !isRightEdge &&
+          squares[i + 1 - width].classList.contains('bomb')
+        )
+          total++;
+
+        if (i < 89 && squares[i + width].contains('bomb')) total++;
+
+        squares[i].setAttribute('data', total);
+      }
+    }
   }
 
   /*calling the create board function*/
   createBoard();
+
+  //clieck function
+  function click(square) {
+    if (square.classList.contains('bomb')) {
+      alert('Game Over');
+    }
+  }
 });
